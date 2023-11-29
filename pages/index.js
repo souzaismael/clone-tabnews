5290624e18df0e519cb9668afb8057e8caac596a
Ismael Souza <80664410+souzaismael@users.noreply.github.com>
2023-11-29 01:53:47 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+function Home() {
+    return (
+        <div>
+            <h1>Para meu caro amigo e irmão Eddie P. Cormac,</h1>
+            <p>
+                Primeiro desafio do curso.dev do Filipe Deschamps. O desafio era criar uma página com uma mensagem
+                para uma pessoa e decidi escolher você!
+            </p>
+            <p>
+                Assim como tô felizão e animado por estar progredindo novamente como dev, gostaria que você também estivesse. Então, mano,
+                espero que você consiga superar todos as barreiras que te impedem de se tornar ainda melhor na nossa área e se tornar um desenvolvedor
+                muito competente e realizado no mundo de desenvolvimento.
+            </p>
+            <p>
+                Pra terminar, uma frase motivacional criada pelo chat GPT:
+            </p>
+            <h2>
+                "Que cada desafio que você enfrenta seja uma oportunidade para mostrar o seu verdadeiro potencial."
+            </h2>
+        </div>
+    )
+}
+
+export default Home; 
