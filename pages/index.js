08ebaafd21a71d5cbb73b353b0b8deba1eba219d
Ismael Souza <80664410+souzaismael@users.noreply.github.com>
2024-07-24 17:39:36 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+function Home() {
+  return (
+    <div>
+      <p>
+        Tem sérios problemas mentais, humor duvidoso, está em algum grau do
+        aspectro autista, além de provalvemente ter algum nível da Síndrome de
+        Tourette mas deixa o dia mais alegre e é minha inspiração. Vos apresento
+        o Pedro:
+      </p>
+      <img src="https://instagram.fgig29-1.fna.fbcdn.net/v/t51.2885-19/450392301_992454689240529_5524648202324518761_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fgig29-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=z7WB9nsZIUwQ7kNvgFREePD&edm=AEhyXUkBAAAA&ccb=7-5&oh=00_AYBUgQCqNxvQAwD1WNE8z7RS8RRnP767DslRwlCMtC3uCQ&oe=66A7213A&_nc_sid=8f1549"></img>
+      <a href="https://www.youtube.com/shorts/2KUGAKpA284">Dedicação</a>
+    </div>
+  );
+}
+
+export default Home;
